7242606f673205eb3a383ece9aa20d5184977abb
Michael.Bomba <44502852+Bombanator@users.noreply.github.com>
2019-04-29 14:05:59 -0400

    attempt to create start button and component
    helped me: https://www.w3schools.com/graphics/game_components.asp

---
+++ Maze.js
@@ -0,0 +1,22 @@
+const startButton = document.querySelector("#startButton")
+
+
+
+startButton.addEventListener("click", startGame);
+
+var KnightQuest;
+
+function startGame() {
+  GameSpace.start();
+  KnightQuest = new component(30, 30, "red", 10, 120);
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y; 
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
