378c1f61fbff95de2fdf8098b1d7ec90f164fa72
Michael.Bomba <44502852+Bombanator@users.noreply.github.com>
2019-05-06 22:28:53 -0400

    trying to get grid to work

--- Maze.js
+++ Maze.js
@@ -60,6 +60,55 @@ function Cell(c, r) {
     rect(x,y,d,d);
   };
   
+  //work with code underneath, to get grid to work//
+  
+  
+var cols, rows;
+var w = 40;
+var grid = [];
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w); 
+  rows = floor(height/w); 
+
+  for (var j = 0; j < rows; j++) {  
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i,j);
+      grid.push(cell);
+    }
+  }
+     
+}       
+                
+function draw() {
+  background(51);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+    
+}
+
+function Cell(i, j) {
+  this.i = i;
+  this.j = j;
+  
+  this.show = function() {
+    var x = this.i*w;
+    var y = this.j*w;
+    stroke(255);
+    line(x,y,x+w,y); //drawing walls
+    line(x+w,y,x+w,y);
+    
+    
+    
+    //noFill();    
+    //rect(x,y,w,w);
+  };
+  
+ 
+}
+  
  
 }
 
