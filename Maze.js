285f20d6f3fe143983fab63cc748fe828748d613
Michael.Bomba <44502852+Bombanator@users.noreply.github.com>
2019-05-06 21:52:46 -0400

    Update Maze.js

--- Maze.js
+++ Maze.js
@@ -21,6 +21,7 @@
   //ctx.fillRect(this.x, this.y, this.width, this.height);
 //}
 
+
 var cols, rows;
 var d = 40; //cells are a square, meaning they have equal dimensions. If their width/height is 40, that mean there are 10 cells per column and row. d means dimension of cell
 var grid = [];
@@ -29,23 +30,20 @@ function CanvasSetup() {
   createCanvas(400, 400);
   cols = floor(width/d); //width of the canvas divided by the width equals number of columns
   rows = floor(height/d); //floor deals with math, makes the console know we are dealing with intergers, numbers are integers
-}
 
   for (var r = 0; r < rows; r++) {   //row generator
     for (var c = 0; c < cols; c++) {
-      var cell = new Cell(c, r);
+      var cell = new Cell(c,r);
       grid.push(cell);
     }
-
+  }
      
 }       
-               
-  
-  
+                
 function draw() {
   background(51);
-  for (var c = 0; c < cells.length; c++) {
-    cells[c].show();
+  for (var c = 0; c < grid.length; c++) {
+    grid[c].show();
   }
     
 }
@@ -53,6 +51,16 @@ function draw() {
 function Cell(c, r) {
   this.c = c;
   this.r = r;
+  
+  this.show = function() {
+    var x = this.c*d;
+    var y = this.r*d;
+    stroke(255);
+    noFill();
+    rect(x,y,d,d);
+  };
+  
+ 
 }
 
 
